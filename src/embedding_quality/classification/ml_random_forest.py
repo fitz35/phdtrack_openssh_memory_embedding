d3cf782db4c652212b83a966ed40eb63294c47c1
Fitz35 <clement@lahoche.info>
2023-09-15 09:14:54 +0200

    fix random seed

--- src/embedding_quality/classification/ml_random_forest.py
+++ src/embedding_quality/classification/ml_random_forest.py
@@ -15,7 +15,7 @@ def ml_random_forest_pipeline(
     A pipeline for training a RandomForestClassifier.
     """
     # Train a RandomForestClassifier
-    clf = RandomForestClassifier(n_estimators=100, random_state=42, n_jobs = params.MAX_ML_WORKERS)
+    clf = RandomForestClassifier(n_estimators=100, random_state=params.RANDOM_SEED, n_jobs = params.MAX_ML_WORKERS)
     clf.fit(samples_and_labels_train.sample, samples_and_labels_train.labels)
     
     # Evaluate model
