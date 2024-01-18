ecabdba478f84d7b9d0596137a23c7957a5d3953
fitz35 <clement@lahoche.info>
2024-01-18 16:39:43 +0100

    vertical array

--- result_analyze/src/word2vec_hyperparameter.py
+++ result_analyze/src/word2vec_hyperparameter.py
@@ -3,20 +3,34 @@ if __name__ == "__main__":
     window_character_sizes = [8, 16]
     word_character_sizes = [2, 4] # size of the word in bytes
 
+    header_line = "\t\\begin{tabular}{|c"
+    outputs_size_line = "\t \\textbf{Embedding dimension} "
+    window_character_size_line = "\t \\textbf{Window character size} "
+    word_character_size_line = "\t \\textbf{Word size} "
 
     print("\\begin{table}[!ht]")
     print("\t\\centering")
-    print("\t\\begin{tabular}{|c|c|c|}")
-    print("\t\\hline")
-    print("\t\\textbf{Word size} & \\textbf{window character size} & \\textbf{Embedding dimension} \\\\ \\hline")
 
     for output_size in outputs_sizes:
         for window_character_size in window_character_sizes:
             for word_character_size in word_character_sizes:
-                    print(f"\t\t{word_character_size} & {window_character_size} & {output_size} \\\\ \\hline")
+                outputs_size_line += f"& {output_size} "
+                window_character_size_line += f"& {window_character_size} "
+                word_character_size_line += f"& {word_character_size} "
+                header_line += "|c"
+
+    outputs_size_line += "\\\\ \\hline"
+    window_character_size_line += "\\\\ \\hline"
+    word_character_size_line += "\\\\ \\hline"
+    header_line += "|}\\hline"
+
+    print(header_line)
+    print(outputs_size_line)
+    print(window_character_size_line)
+    print(word_character_size_line)
 
     print("\t\\end{tabular}")
-    print("\t\\caption{Hyperparameters for the Transformers model.}")
-    print("\t\\label{tab:transformers_hyperparams}")
+    print("\t\\caption{Hyperparameters for the Word2Vec model.}")
+    print("\t\\label{tab:Word2Vec_hyperparams}")
     print("\\end{table}")
                     
