b652d01f20b9c1a653a9347afd3fe80fb1ba124e
fitz35 <clement@lahoche.info>
2024-01-19 15:36:34 +0100

    update latex generation

--- result_analyze/src/transformer_hyperparameter.py
+++ result_analyze/src/transformer_hyperparameter.py
@@ -10,18 +10,45 @@ if __name__ == "__main__":
 
     zipped = zip(zip(zip(transformer_units, num_heads), num_transformer_layers), dropout_rates)
 
+    header_lines = "\t\\begin{tabular}{|c"
+    word_character_size_line = "\t \\textbf{Word size} "
+    embedding_dim_line = "\t \\textbf{Embedding dimension} "
+    transformer_unit_line = "\t \\textbf{Transformer units} "
+    num_head_line = "\t \\textbf{Num heads} "
+    num_transformer_layer_line = "\t \\textbf{Num layers} "
+    dropout_rate_line = "\t \\textbf{Dropout rate} "
+
     print("\\begin{table}[!ht]")
     print("\t\\centering")
-    print("\t\\begin{tabular}{|p{2cm}|p{2cm}|p{2cm}|p{2cm}|p{2cm}|p{2cm}|}")
-    print("\t\\hline")
-    print("\t \\textbf{Word size} & \\textbf{Embedding dimension} & \\textbf{Transformer units} & \\textbf{Num heads} & \\textbf{Num layers} &  \\textbf{Dropout rate}  \\\\ \\hline")
 
     for (((transformer_unit, num_head), num_transformer_layer), dropout_rate) in zipped:
         for word_character_size in word_character_sizes:
             for embedding_dim in embedding_dims:
             
                 for activation in activations:
-                    print(f"\t\t {word_character_size} & {embedding_dim} & {transformer_unit} & {num_head} &  {num_transformer_layer} &  {dropout_rate} \\\\ \\hline")
+                    header_lines += "|c"
+                    word_character_size_line += f"& {word_character_size} "
+                    embedding_dim_line += f"& {embedding_dim} "
+                    transformer_unit_line += f"& {transformer_unit} "
+                    num_head_line += f"& {num_head} "
+                    num_transformer_layer_line += f"& {num_transformer_layer} "
+                    dropout_rate_line += f"& {dropout_rate} "
+     
+    word_character_size_line += "\\\\ \\hline"
+    embedding_dim_line += "\\\\ \\hline"
+    transformer_unit_line += "\\\\ \\hline"
+    num_head_line += "\\\\ \\hline"
+    num_transformer_layer_line += "\\\\ \\hline"
+    dropout_rate_line += "\\\\ \\hline"
+    header_lines += "|}\\hline"
+
+    print(header_lines)
+    print(word_character_size_line)
+    print(embedding_dim_line)
+    print(transformer_unit_line)
+    print(num_head_line)
+    print(num_transformer_layer_line)
+    print(dropout_rate_line)
 
     print("\t\\end{tabular}")
     print("\t\\caption{Hyperparameters for the Transformers model.}")
